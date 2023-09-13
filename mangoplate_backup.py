876abec39c3a8cefdb84ee03271e3db45ead9784
Eliza Yang <yjh_612@naver.com>
2023-09-13 11:36:13 +0900

    기능 변경
    pandas dataframe 사용시 용량이 너무 커, csv 읽기로 기능을 변경하였습니다.

--- mangoplate_backup.py
+++ mangoplate_backup.py
@@ -1,19 +1,19 @@
 '''
 csv 열 설명
-1 : 일련번호
-2 : 식당이름
-3 : 식당지점이름
-4 : 식당주소
-5 : 식당평점
-6 : 리뷰평점
-7 : 리뷰본문
-8 : 리뷰작성일시
-9 ~ 38 : 사진
+0 : 일련번호
+1 : 식당이름
+2 : 식당지점이름
+3 : 식당주소
+4 : 식당평점
+5 : 리뷰평점
+6 : 리뷰본문
+7 : 리뷰작성일시
+8 ~ 37 : 사진
 '''
 
-import pandas as pd
 import sys, os
 import urllib.request
+import csv
 
 import tkinter as tk
 from tkinter import filedialog
@@ -34,6 +34,14 @@ def openfile():
         ("CSV", "*.csv"), ("모든 파일", "*.*")
     ))
 
+    while True:
+        if filename != '':
+            break
+        root.update()
+        
+    label_afterLoad = tk.Label(root, text = filename, width=50, height=2)
+    label_afterLoad.grid(row=0, column=0, columnspan=15, padx = 5, pady=20)
+    
 '''
 "백업 시작" 버튼을 클릭하면 전체 행에 대한 반복
 1. 폴더 생성
@@ -42,37 +50,42 @@ def openfile():
 '''
 def startBackup():
     global filename, status
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        rdr = csv.reader(f)
+        next(rdr)
+
+        for line in rdr:
+            # line 리스트 길이
+            end = len(line)
+            
+            # csv 에서 사용할 이름 변수 만들기
+            restname = line[1].replace(" ","_")
+            if isinstance(line[2], str):
+                branchname = line[2].replace(" ","_")
+            else:
+                branchname = ""
+            rvdate = str(line[7])[:10]
+
+            # 폴더 이름 짓기
+            foldername = restname + branchname + rvdate + "_" + str(line[0])
+
+            # 폴더 생성
+            new_Folder = "나의 망고플레이트 기록/" + foldername
+            os.makedirs(new_Folder, exist_ok=True)
     
-    df = pd.read_csv(filename)
-    os.makedirs("나의 망고플레이트 기록", exist_ok=True)
+            # 리뷰 텍스트 다운로드
+            rvtxt = open(new_Folder + '/' + foldername + '.txt', 'w', encoding="utf-8")
+            rvtxt.write(line[6])
+            rvtxt.close()
 
-    for row in df.itertuples():
-        # 폴더 이름 짓기
-        restname = row[2].replace(" ", "_")
-        if isinstance(row[3], str):
-            branchname = row[3].replace(" ", "_")
-        else:
-            branchname=""
-        rvdate = str(row[8])[:10]
-
-        foldername = restname + branchname + rvdate + str(row[1])
-
-        # 폴더 생성
-        new_Folder = "나의 망고플레이트 기록/" + foldername
-        os.makedirs(new_Folder, exist_ok=True)
-
-        # 리뷰 텍스트 다운로드
-        rvtxt = open(new_Folder + '/' + foldername + '.txt', 'w', encoding="utf-8")
-        rvtxt.write(row[7])
-        rvtxt.close()
-
-        # 리뷰 이미지 다운로드
-        # row 8~37
-        img = 1
-        for i in range(9, 38):
-            if isinstance(row[i], str):
-                urllib.request.urlretrieve(row[i], new_Folder + '/' + restname + str((i-8)) + ".jpg")
-
+            # 리뷰 이미지 다운로드
+            # row 8~37
+            img = 1
+            for i in range(8, end):
+                if isinstance(line[i], str):
+                    urllib.request.urlretrieve(line[i], new_Folder + '/' + restname + str((i-7)) + ".jpg")
+    
 if __name__ == "__main__":
     # 로드 버튼
     btn_Load = tk.Button(root, text="파일 불러오기", command=openfile, width=14, height = 2)
@@ -83,16 +96,9 @@ if __name__ == "__main__":
     label_afterLoad.grid(row=0, column=1, padx = 5, pady=20)
 
     # 백업 버튼
-    btn_Backup = tk.Button(root, text = "백업", command=startBackup, width = 15, height = 2)
+    btn_Backup = tk.Button(root, text = "백업 시작", command=startBackup, width = 15, height = 2)
     btn_Backup.grid(row=2, column=1, pady = 1)
 
-    while True:
-        if filename != '':
-            break
-        root.update()
-
-    label_afterLoad = tk.Label(root, text = filename, width=50, height=2)
-    label_afterLoad.grid(row=0, column=0, columnspan=15, padx = 5, pady=20)
 
     # 메인함수 tkinter 실행
     root.mainloop()
